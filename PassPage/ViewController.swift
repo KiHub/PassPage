3e121fd89473e6279d5d99d571f161af259cffae
Kirill Ki <kirillkalapov@gmail.com>
2021-07-30 16:49:59 +0200

    Initial Commit

---
+++ PassPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassPage
+//
+//  Created by  Mr.Ki on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
